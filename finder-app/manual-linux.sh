83b187d10ebeb35e7f2a9cab764fd9a4eb348340
Ben Appold <benjamin.appold@colorado.edu>
2024-03-02 17:44:07 -0600

    Assignment 3 Part 2 Upload for Testing #1

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,7 +35,21 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "TEST 2"    
+    #cp /boot/config-$(uname -r) .config #learned from Ubuntu Wiki
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig #from lecture video 
+    echo "TEST 3"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "TEST 4"
+    # do I need to add modules here? skipping per instructions
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "TEST 5"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    echo "TEST 6"     
 fi
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR} #copy the boot image to OUTDIR
+
 
 echo "Adding the Image in outdir"
 
@@ -48,6 +62,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd -p ${OUTDIR}/rootfs
+mkdir -p {bin, dev, etc, home, lib, lib64, proc, sbin, sys, tmp, usr/{bin, lib, sbin}, var/log}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +73,49 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
+    
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT_DIR=$(realpath $(${CROSS_COMPILE}gcc --print-sysroot))
+#found from Linux Root Filesystems lecture 10:00
+cp ${SYSROOT_DIR}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT_DIR}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT_DIR}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT_DIR}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3 
+sudo mknod -m 666 dev/vonsole c 5 1 
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
